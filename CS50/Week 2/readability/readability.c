5533ef1a8c40e3a2c52ace282d80093df1299b1e
Brandon Egna <brandon.egna@outlook.com>
2025-04-21 14:00:27 -0400

    Finished Readability

--- CS50/Week 2/readability/readability.c
+++ CS50/Week 2/readability/readability.c
@@ -4,26 +4,28 @@ typedef char *string;
 
 
 long unsigned int strlen(string string);
+int int_round(float number);
+int isalpha(int character);
 int count_letters(string user_text);
 int count_words(string user_text);
 int count_sentences(string user_text);
 
 int main(void){
     // TODO get a string from the user
-    string text;
+    char text[65536];
     printf("What is your text? ");
-    scanf("%s", &text);
+    fgets(text, sizeof(text), stdin);
     // TODO Run the string through the three functions that calculate the letters, words, and sentences in the string
     float L = (float) count_letters(text) / count_words(text) * 100;
     float S = (float) count_sentences(text) / count_words(text) * 100;
     // TODO Calculate the Reading level:
     float index = 0.0588 * L - 0.296 * S - 15.8;
     // TODO Print "Grade x" where x is the grade level of the text according to the Coleman-Liau index
-    if (round(index) > 1 && round(index) < 16)
+    if (int_round(index) > 1 && int_round(index) < 16)
     {
-        printf("Grade %i\n", (int) round(index));
+        printf("Grade %i\n", (int) int_round(index));
     }
-    else if (round(index) < 1)
+    else if (int_round(index) < 1)
     {
         printf("Before Grade 1\n");
     }
@@ -40,14 +42,49 @@ long unsigned int strlen(string string){
     return i;
 }
 
-int count_letters(string user_text){
+int int_round(float number){
+    int rounded = (int)(number < 0 ? (number - 0.5) : (number + 0.5));
+    return rounded;
+}
 
+int isalpha(int character){
+    if ((character >= 65 && character <= 90) || (character >= 97 && character <= 122)){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
+
+int count_letters(string user_text){
+    int letter_amount = 0;
+    for (int i = 0; i < strlen(user_text); i++){
+        if (isalpha(user_text[i]) == 1){
+            letter_amount += 1;
+        }
+    }
+
+    return letter_amount;
 }
 
 int count_words(string user_text){
+    int word_amount = 1;
+    for (int i = 0; i < strlen(user_text); i++){
+        if (user_text[i] == ' '){
+            word_amount += 1;
+        }
+    }
 
+    return word_amount;
 }
 
 int count_sentences(string user_text){
+    int sentence_amount = 0;
 
+    for (int i = 0; i < strlen(user_text); i++){
+        if (user_text[i] == '.' || user_text[i] == '?' || user_text[i] == '!'){
+            sentence_amount += 1;
+        }
+    }
+    return sentence_amount;
 }
